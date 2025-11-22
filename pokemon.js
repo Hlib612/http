87f14e9dca192973a83074d9bc87af6da288858e
Hlib612 <glebmalonos@gmail.com>
2025-11-22 11:09:14 +0200

    add some

--- pokemon.js
+++ pokemon.js
@@ -2,6 +2,15 @@ const searchForm = document.querySelector(".js-search-form");
 const contrulInput = document.querySelector(".form-control");
 const cardContainer = document.querySelector(".js-card-container");
 const BASE_URL = "https://pokeapi.co/api/v2/";
+const pokemonTamplate = document.querySelector("#pokemon-templ")
+
+// console.log(pokemonTamplate);
+
+const renderPokemon = (pokemon) =>{
+  const template = Handlebars.compile(pokemonTamplate.innerHTML);
+  const murkup = template(pokemon);
+  cardContainer.innerHTML = murkup;
+}
 
 const fetchPokemon = (id) => {
   return fetch(`${BASE_URL}pokemon/${id}`).then((response) => {
@@ -19,10 +28,17 @@ const onFormSubmit = (event) => {
   // console.log(event.currentTarget)
   // console.log(event.currentTarget.elements)
   // console.log(event.currentTarget.elements.query)
+  if(query.trim() === ""){
+    return
+  }
   console.log(event.currentTarget.elements.query.value);
   fetchPokemon(query).then((pokemon) => {
-    cardContainer.textContent = pokemon.name;
-  });
+    renderPokemon(pokemon)
+  })
+  .catch(error => console.log(error))
+  .finally(() => {
+searchForm.reset()
+  })
 };
 
 searchForm.addEventListener("submit", onFormSubmit);
